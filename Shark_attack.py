e73aa51451c97933677c1289a93ee657d0f9d55e
Dazzled <gaurikabhatt27@gmail.com>
2025-04-12 18:13:36 +0530

    Initial commit with Shark Attacks Project

---
+++ Shark_attack.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+from matplotlib.ticker import MaxNLocator
+
+df = pd.read_csv(r"C:\Users\gauri\OneDrive\Desktop\python 4th sem\CA2\attacks.csv (1)\attacks.csv", encoding="latin1")
